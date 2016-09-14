553962d3295ccf3888d3f1b9a1a591a5d413a8ba
cowthan <cowthan2013@gmail.com>
2016-09-14 11:20:15 +0800

    first commit

---
+++ ayo-eventbus/doc.md
@@ -0,0 +1,7 @@
+# AyoEventBus
+-------------------------
+
+这个就是直接把EventBus拷来了，如果版本不是最新，应该随时替换
+https://github.com/greenrobot/EventBus
+
+直接看doc-eventbus.md
---
+++ ayo-eventbus/src/test/java/org/greenrobot/eventbus/ExampleUnitTest.java
@@ -0,0 +1,10 @@
+package org.greenrobot.eventbus;
+
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    public void addition_isCorrect() throws Exception {
+    }
+}
